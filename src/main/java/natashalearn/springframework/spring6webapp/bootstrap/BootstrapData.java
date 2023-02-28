5c21fdb00f30b83a1ebcc13c0f07f329729fe73e
Natasha <natashajanicetambunan@gmail.com>
2023-02-28 17:17:26 +0700

    Add publisher JPA entity, repositories, and boostrap

--- src/main/java/natashalearn/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/natashalearn/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package natashalearn.springframework.spring6webapp.bootstrap;
 
 import natashalearn.springframework.spring6webapp.domain.Author;
 import natashalearn.springframework.spring6webapp.domain.Book;
+import natashalearn.springframework.spring6webapp.domain.Publisher;
 import natashalearn.springframework.spring6webapp.repositories.AuthorRepository;
 import natashalearn.springframework.spring6webapp.repositories.BookRepository;
+import natashalearn.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -49,7 +53,17 @@ public class BootstrapData implements CommandLineRunner {
         authorRepository.save(rodSaved);
 
         System.out.println("In Bootstrap");
-        System.out.println("Author Count" + authorRepository.count());
-        System.out.println("Book Count" + bookRepository.count());
+        System.out.println("Author Count: " + authorRepository.count());
+        System.out.println("Book Count: " + bookRepository.count());
+
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("Publisher Name");
+        publisher.setAddress("Jl. MH Thamrin");
+        publisher.setCity("Jakarta");
+        publisher.setState("DKI Jakarta");
+        publisher.setZip("15001");
+
+        Publisher publisherSaved = publisherRepository.save(publisher);
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
---
+++ src/main/java/natashalearn/springframework/spring6webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package natashalearn.springframework.spring6webapp.repositories;
+
+import natashalearn.springframework.spring6webapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
