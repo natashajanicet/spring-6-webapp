0bd9f9cbfd786b24b204140d2a2e28521371187d
Natasha <natashajanicetambunan@gmail.com>
2023-03-01 14:38:58 +0700

    Create controller of book to handle request mapping

---
+++ src/main/java/natashalearn/springframework/spring6webapp/controllers/BookController.java
@@ -0,0 +1,21 @@
+package natashalearn.springframework.spring6webapp.controllers;
+
+import natashalearn.springframework.spring6webapp.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookService.findAll());
+        return "books";
+    }
+}
