316c6328834d1c6d3fc015cf5fc50566b459309a
Natasha <natashajanicetambunan@gmail.com>
2023-03-01 15:07:14 +0700

    Add thymeleaf dependency, add books.html and map with thymeleaf syntax to read the passed data

--- src/main/java/natashalearn/springframework/spring6webapp/controllers/BookController.java
+++ src/main/java/natashalearn/springframework/spring6webapp/controllers/BookController.java
@@ -16,6 +16,6 @@ public class BookController {
     @RequestMapping("/books")
     public String getBooks(Model model) {
         model.addAttribute("books", bookService.findAll());
-        return "books";
+        return "books"; // to refer to books.html in resources/template folder
     }
 }
